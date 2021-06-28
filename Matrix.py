9549072c586344a0774c80837896762b6c72c2bc
SilviaMariaMacrì <silviamaria.macri@studio.unibo.it>
2021-06-28 13:08:16 +0200

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 from scipy.spatial.distance import cdist
-from itertools import combinations
-
 
 
 
@@ -13,99 +11,86 @@ def DistanceMatrix(X):
 	
 	if isinstance(X,pd.DataFrame):
 		X = np.array(X)
-		
 	
-	#associo indice a ciascun punto
 	n_d = len(X[0]) # numero dimensioni
-	data = pd.DataFrame(X)
-	data.columns = [*[f'dim{s}_point' for s in data.columns]]
-	data['index_point'] = data.index
-
-
-	#dataframe di coppie di punti
-	pair_points = list(combinations(np.arange(len(X)), 2))
-	pair_points = pd.DataFrame(pair_points)
-	pair_points.columns = ['index_point_x','index_point_y']
-
 	
 	
+	dist = cdist(X,X)
 	
-	dist = pd.DataFrame(cdist(X,X))
-
-	p1 = []
-	p2 = []
-	d = []
-	for i,j in zip(pair_points['index_point_x'],pair_points['index_point_y']):
-		p1.append(i)
-		p2.append(j)
-		d.append(dist.iloc[i,j])
+	i1, i2 = np.tril_indices(len(X), k=-1) 
 	
-		
-	pair_points = {'index_point_x':p1,'index_point_y':p2,'dist':d}
-	pair_points = pd.DataFrame(pair_points)
-
-	
-	#associo punti alle coppie di indici 
-	pair_points = pd.merge(pair_points,data, how='left', left_on='index_point_x', right_on='index_point')
-	pair_points = pair_points.drop('index_point',axis=1)
-	pair_points = pd.merge(pair_points,data, how='left', left_on='index_point_y', right_on='index_point')
-	pair_points = pair_points.drop('index_point',axis=1)
-	
-	pair_points = pair_points[['index_point_x', 'index_point_y', 'dim0_point_x', 'dim1_point_x', 'dim0_point_y', 'dim1_point_y', 'dist']]
-
-
-	names = []
-	#punto medio congiungente i due punti (cut)
+	# coppie di punti
+	X1 = []
+	X2 = []
+	X_cut = []
 	for i in range(n_d):
-		pair_points['cut_point'+str(i)] = (pair_points['dim'+str(i)+'_point_x'] + pair_points['dim'+str(i)+'_point_y'])/2
-		#vettore normale al taglio (parallelo a congiungente i due punti)
-		pair_points['vettore_'+str(i)] = pair_points['dim'+str(i)+'_point_y'] - pair_points['dim'+str(i)+'_point_x']
-		names.append('vettore_'+str(i))
+		x1 = np.take(X[:,i],i1)
+		x2 = np.take(X[:,i],i2)
+		X1.append(x1)
+		X2.append(x2)
+		X_cut.append((x1+x2)/2)
+
+	coord1 = pd.DataFrame(X1).T
+	coord1.columns = [*['x1_'+str(i) for i in range(n_d)]]
+
+	coord2 = pd.DataFrame(X2).T
+	coord2.columns = [*['x2_'+str(i) for i in range(n_d)]]
+	
+	coord_cut = pd.DataFrame(X_cut).T
+	coord_cut.columns = [*['x_cut_'+str(i) for i in range(n_d)]]
 	
 	
-	#normalizzo vettore normale
+	# versore piano perpendicolare a distanza
+	versori_coord = []
 	for i in range(n_d):
-		pair_points.loc[pair_points.index,'norm_vect_'+str(i)] = [*[(pair_points['vettore_'+str(i)].iloc[j]/np.linalg.norm(pair_points[names].iloc[j])) for j in range(len(pair_points)) ]]
-	
-	names_norm_vect = []
-	names_cut = []
-	for i in range(n_d):
-		pair_points = pair_points.drop('vettore_'+str(i),axis=1)
-		names_norm_vect.append('norm_vect_'+str(i))
-		names_cut.append('cut_point'+str(i))
+		d = np.subtract.outer(X[:,i],X[:,i]) 
+		d_norm = d[i1, i2]/dist[i1, i2]
+		versori_coord.append(d_norm)
 		
-	#modulo vettore normale
-	pair_points.loc[pair_points.index,'magnitude_norm_vect'] = [*[np.dot(pair_points[names_norm_vect].iloc[j],pair_points[names_cut].iloc[j]) for j in range(len(pair_points))]]
+	versori_iperpiani_df = pd.DataFrame(versori_coord).T
+	versori_iperpiani_df.columns = [*['norm_vect_'+str(i) for i in range(n_d)]]
+					
+	versori_iperpiani = np.array(versori_iperpiani_df)
+	
+	magnitude_norm_vect = versori_iperpiani@(np.array(coord_cut).T)
+	magnitude_norm_vect = np.diag(magnitude_norm_vect)
+	
+	matrix = np.stack([i1.astype(int), i2.astype(int), dist[i1, i2],magnitude_norm_vect]).T
+	matrix = pd.DataFrame(matrix)
+	matrix.columns = ['index1','index2','dist','magnitude_norm_vect']
+	matrix['index1'] = matrix['index1'].astype(int)
+	matrix['index2'] = matrix['index2'].astype(int)
+	
+	matrix['index_norm_vect'] = matrix.index
+
+	for i in [coord1,coord2,coord_cut,versori_iperpiani_df]:
+		matrix = pd.merge(matrix,i, left_index=True, right_index=True)
 	
 
 
-	dist_matrix = pd.DataFrame()
-	
-	for i in range(len(data)):
+	matrix_copy = pd.DataFrame()
+	for i in range(len(X)):
+		print(i)
 		
-		matrix = pair_points.copy()
+		for j in range(len(X[i])):
+			matrix['point_'+str(j)] = X[i,j]
+		matrix['point_index'] = i
 		
-		for j in data:
-			matrix[str(j)] = data[str(j)].iloc[i]
-		#matrix = matrix.drop(matrix.query('index_point_x==index or index_point_y==index').index)
-		#matrix = matrix.drop('index_point',axis=1)
+		matrix_copy = pd.concat([matrix,matrix_copy])
+	matrix = matrix_copy.copy()
+	matrix.index = np.arange(len(matrix))
 
-		dist_matrix = pd.concat([dist_matrix,matrix])
-		
-	names_point = []	
-	for i in range(n_d):
-		names_point.append('dim'+str(i)+'_point')
-		
-	dist_matrix.index = np.arange(len(dist_matrix))
+	i1_bis = np.array(matrix['index_norm_vect'])
+	i2_bis = np.array(matrix['point_index'])
 	
 	
-	#calcolo distanza punti da retta cut
-	dist_matrix.loc[dist_matrix.index,'distance_point_cut'] = [*[(np.dot(dist_matrix[names_norm_vect].iloc[j],dist_matrix[names_point].iloc[j]) - dist_matrix['magnitude_norm_vect'].iloc[j]) for j in range(len(dist_matrix))]]
+	dist_point_cut = versori_iperpiani@X.T
+	dist_point_cut = dist_point_cut[i1_bis,i2_bis] - np.array(matrix['magnitude_norm_vect'])
+	matrix['dist_point_cut'] = dist_point_cut
 	
+	
+	
+	return matrix
 
 
-
-
-
-
-	return dist_matrix
+	
